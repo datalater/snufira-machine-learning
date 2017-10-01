c064a98b20ca03ef0fcc3c2fe9e4527d79415421
datalater <the7mincheol@gmail.com>
2017-10-01 19:12:29 +0900

    report_v5 #inProgress

--- lab/lab6/W6/K-means_basic.py
+++ lab/lab6/W6/K-means_basic.py
@@ -7,4 +7,3 @@ kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
 print "Clusters %s"%(kmeans.labels_)
 print "Cluster centroids: %s"%(kmeans.cluster_centers_)
 print "Prediction cluster of [0, 0], [4, 4]: %s"%(kmeans.predict([[0, 0], [4, 4]]))
-
